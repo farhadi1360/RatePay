95a3c6d72885eae132d06957490598125103d5b0
farhadi1360 <farhadi.kam@gmail.com>
2022-08-13 21:28:44 +0430

    this is first commit

---
+++ client/bugtracker/src/main/java/com/ratepay/client/bugtracker/package-info.java
@@ -0,0 +1,1 @@
+package com.ratepay.client.bugtracker;
---
+++ microservices/bugtracker-services/src/main/java/com/ratepay/bugtracker/bugtrackerdemo/BugtrackerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ratepay.bugtracker.bugtrackerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugtrackerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugtrackerDemoApplication.class, args);
+	}
+
+}
